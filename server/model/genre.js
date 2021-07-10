67605511c704b4de09c862c5cab47d3887d33222
itsjaenae <78134531+itsjaenae@users.noreply.github.com>
2021-07-10 10:32:36 +0100

    Delete server directory

--- server/model/genre.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const genreSchema = new mongoose.Schema({
-    genre: { type: String, required: true, unique: true }
-});
-
-module.exports = mongoose.model('Genre', genreSchema);
--- server/routes/genre.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-const { getGenres } = require('../controller/genre');
-
-router.get('/', getGenres);
-
-module.exports = router;
